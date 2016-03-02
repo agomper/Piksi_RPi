e7262b05cb2abf8c59f5bfe2055763b83e5f9c33
Alejandro Gomez <agomper@gmail.com>
2016-03-02 16:10:39 +0100

    Piksi RPi UART Test. Working and showing data.

--- main.cpp
+++ main.cpp
@@ -18,10 +18,25 @@
 #include <fcntl.h>			//Used for UART
 #include <termios.h>                    //Used for UART
 #include <poll.h>                       //Used for UART
-#include <queue>
+#include <cstring>
+//#include <queue>
+//#include "queuelist.h"
+
+typedef unsigned char 		u8;
+typedef unsigned short 		u16;
+typedef unsigned int 		u32;
+
+#define PREAMBLE 0x55
+#define MESG_1 0x01
+#define MESG_2 0x02
+#define ID_1 0x23
+#define ID_2 0x6a
+#define PayloadLength 34
+
 using namespace std;
 struct pollfd pfd[1];
-queue <unsigned char> PackageQueue;
+//queue <unsigned char> PackageQueue;
+unsigned char Payload[PayloadLength];
 
 
 void uart_setup() {
@@ -75,63 +90,128 @@ void uart_setup() {
 	tcsetattr(pfd[0].fd, TCSANOW, &options);
 }
 
-void show_queue() { //It shows the whole message from Preamble to CRC
-    while (PackageQueue.empty() == false) {
-        printf ("%x ", PackageQueue.front());
-        PackageQueue.pop();
-    }
-    printf("\n");
+void show_queue() { 
+    u8 n_sats, flags;
+    u32 tow;
+    double latitude, longitude, height;
+    
+    unsigned char tow_bytes[4];
+    memcpy(tow_bytes, &Payload[0], 4*sizeof(unsigned char));
+    memcpy(&tow, &Payload[0], 4*sizeof(unsigned char));
+    
+    //unsigned char latitude_bytes[8];
+    //memcpy(latitude_bytes, &Payload[4], 8*sizeof(unsigned char));
+    memcpy(&latitude, &Payload[4], 8*sizeof(unsigned char));
+    
+    //unsigned char longitude_bytes[8];
+    //memcpy(longitude_bytes, &Payload[12], 8*sizeof(unsigned char));
+    memcpy(&longitude, &Payload[12], 8*sizeof(unsigned char));
+    
+    memcpy(&height, &Payload[20], 8*sizeof(unsigned char));
+    memcpy(&n_sats, &Payload[32], 1*sizeof(unsigned char));
+    memcpy(&flags, &Payload[13], 1*sizeof(unsigned char));
+    
+    //for (int i=4; i<12; i++)
+    //    printf ("%x ", tow_bytes[i]);
+    //    
+    //printf("\n");
+    printf("Latitude: %Lf   Longitude: %Lf  Height: %Lf   Sats: %d \n", 
+            latitude, longitude, height, n_sats);
+    
+    /*for (int i=0; i<PayloadLength; i++)
+        printf ("%x ", Payload[i]);
+        
+    printf("\n");*/
 }
 
 
-void clean_queue() { //Erase the queue
-    while (PackageQueue.empty() == false) {
-        PackageQueue.pop();
-    }
-}
 
 void process_byte (unsigned char inByte) {
-   static int PackageLength = 100;
-   static bool NewPackage, MessageCode1, MessageCode2 = false;
+   static int PackageLength = 0/*, IteratorControl = 10*/;
+   static bool PreambleFound = false, Message1Found = false, 
+               Message2Found = false, ID1Found = false, ID2Found = false;
    //printf ("Procesando %x\n", inByte);
    
-   if (inByte == 0x55) {                //SBP Preamble
-      //Serial.print("New Package!");
-      printf("New Package\n");
-      NewPackage = true;
-      PackageQueue.push(inByte);        //Insert the byte in the queue
+   while (PackageLength > 0/* && IteratorControl == 0*/) {
+       Payload[abs(PackageLength-PayloadLength)] = inByte;
+       PackageLength--;
+       break;
    }
-   else if (NewPackage == true) {
-      if (PackageQueue.size() == 1 && inByte == 0x02 ) {     //Code HEX Message 1 - Message type are in positions 1 and 2
-        printf("MessageCode1 True\n");
-        MessageCode1 = true;
-        PackageQueue.push(inByte);      //Insert the byte in the queue
-      }
-      else if (PackageQueue.size() == 2 && inByte == 0x01 && MessageCode1 == true) {  //Code HEX Message 2
-        printf("MessageCode2 True\n");
-        MessageCode2 = true;
-        PackageQueue.push(inByte);      //Insert the byte in the queue
-      }
-      else if (MessageCode2 == true) {  //Message Type Match
-        if (PackageQueue.size() == 5) {  //Length byte in position 5
-            PackageLength = 34/*inByte*/ + 8;   //Length = Payload + 8 = Fixed bytes (Preamble, Type, Sender, Length and CRC)
-        }
-          
-        PackageQueue.push(inByte);
-      }
-      else if (MessageCode2 == false){ //No match message type. Erase.
-        clean_queue();
-        NewPackage = false;
-        MessageCode1 = false;
-      }
+   
+   if (inByte == PREAMBLE) {
+       PreambleFound = true;
    }
-
-   if (PackageQueue.size() == PackageLength) { //Got all the package. Payload + 8
-      show_queue();
-      //process_message(PackageQueue.size()); //Process Message
-      NewPackage = false;
-      MessageCode1 = MessageCode2 = false; //Reset values
+   else if (PreambleFound == true && inByte == MESG_1) {
+       Message1Found = true;
    }
+   else if (Message1Found == true && inByte == MESG_2) {
+       Message2Found = true;
+   }
+   else if (Message2Found == true && inByte == ID_1) {
+       ID1Found = true;
+   }
+   else if (ID1Found == true && inByte == ID_2) {
+       ID2Found = true;
+   }
+   else if (ID2Found == true){
+       PackageLength = inByte;
+       //printf ("Nuevo paquete con %x\n", inByte);
+       PreambleFound = false;
+       Message1Found = false;
+       Message2Found = false;
+       ID1Found = false;
+       ID2Found = false;
+       PackageLength = inByte;
+       //IteratorControl--;
+       show_queue();
+   }
+   else {
+       PreambleFound = false;
+       Message1Found = false;
+       Message2Found = false;
+       ID1Found = false;
+       ID2Found = false;
+   }
+   
+   
+    
+   
+//   if (inByte == 0x55) {                //SBP Preamble
+//      //printf("New Package\n");
+//      NewPackage = true;
+//      PackageQueue.push(inByte);        //Insert the byte in the queue
+//   }
+//   else if (NewPackage == true) {
+//      if (PackageQueue.size() == 1 && inByte == 0x01 ) {     //Code HEX Message 1 - Message type are in positions 1 and 2
+//        //printf("MessageCode1 True\n");
+//        MessageCode1 = true;
+//        PackageQueue.push(inByte);      //Insert the byte in the queue
+//      }
+//      else if (PackageQueue.size() == 2 && inByte == 0x02 && MessageCode1 == true) {  //Code HEX Message 2
+//        //printf("MessageCode2 True\n");
+//        MessageCode2 = true;
+//        PackageQueue.push(inByte);      //Insert the byte in the queue
+//      }
+//      else if (MessageCode2 == true) {  //Message Type Match
+//        if (PackageQueue.size() == 5) {  //Length byte in position 5
+//            PackageLength = inByte + 8;   //Length = Payload + 8 = Fixed bytes (Preamble, Type, Sender, Length and CRC)
+//        }
+//          
+//        PackageQueue.push(inByte);
+//      }
+//      else if (MessageCode2 == false){ //No match message type. Erase.
+//        clean_queue();
+//        NewPackage = false;
+//        MessageCode1 = false;
+//      }
+//   }
+//
+//   if (PackageQueue.size() == PackageLength) { //Got all the package. Payload + 8
+//      show_queue();
+//      //process_message(PackageQueue.size()); //Process Message
+//      NewPackage = false;
+//      MessageCode1 = MessageCode2 = false; //Reset values
+//   }
 }
 
 
